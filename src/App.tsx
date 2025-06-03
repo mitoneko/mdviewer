62889c9da8aaf8035cab986f37d3c3fed38ecb09
mitoneko <facemito@laki.jp>
2025-06-03 21:54:52 +0900

    html-react-parserのテスト
    トップ画面の仮構造設定
    html-react-parserのテスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
-import { Text } from "@yamada-ui/react";
+import { Container, Box } from "@yamada-ui/react";
+import parse from "html-react-parser";
 
-function App() {
+export default function App() {
 
   return (
-      <Text>まっさらのページ</Text>
+      <Container width="100dvw" height="100dvh" margin="0" padding="0" gap="0">
+          <Box height="4em" bg="#FFBA84"></Box>
+          <Box height="calc(100% - 4em)" >
+              <Contents />
+          </Box>
+      </Container>
   );
 }
 
-export default App;
+function Contents() {
+    const contents = "<h1>工事中</h1> <p> ただいま、<strong>工事中</strong>です。 </p>";
+
+    return (
+        <>
+            {parse(contents)}
+        </>
+    );
+}
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { UIProvider } from "@yamada-ui/react";
+import { UIProvider, extendTheme, UIStyle } from "@yamada-ui/react";
+
+const globalStyle: UIStyle = {
+    body: {
+        bg: "#ECB88A",
+    },
+}
+
+const customTheme = extendTheme({ styles: { globalStyle } })()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <UIProvider>
+      <UIProvider theme={customTheme} >
         <App />
     </UIProvider>
   </React.StrictMode>,
