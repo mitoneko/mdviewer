c59816c8175313e5a455a1e35dfe7180d8bd8af8
mitoneko <facemito@laki.jp>
2025-06-15 10:29:39 +0900

    ツールバーの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
-import { Container, Box } from "@yamada-ui/react";
+import { Container, Box, IconButton, Wrap} from "@yamada-ui/react";
+import { MdOutlineFileOpen } from "react-icons/md";
 
 import Contents from "./Contents";
 
@@ -8,10 +9,20 @@ export default function App() {
 
   return (
       <Container width="100dvw" height="100dvh" margin="0" padding="0" gap="0">
-          <Box height="4em" bg="#FFBA84"></Box>
+          <Container bg="#FFBA84" height="4em" gap="0" padding="0">
+              <Toolbar />
+          </Container>
           <Box height="calc(100% - 4em)" >
               <Contents />
           </Box>
       </Container>
   );
+}
+
+function Toolbar() {
+    return (
+        <Wrap align="center" paddingX="0.5em" marginY="auto">
+            <IconButton variant="outline" icon={<MdOutlineFileOpen />} size="md" />
+        </Wrap>
+    );
 }
