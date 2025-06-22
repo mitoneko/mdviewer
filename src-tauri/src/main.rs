0e196eb68eba782c05536bb7c9c0800db6b78dcc
mitoneko <facemito@laki.jp>
2025-06-22 17:56:59 +0900

    ファイルの読み込みの実装。
    - src-tauri/src/args.rs: コマンドライン引数の処理を追加。
    - src-tauri/src/document.rs:
      ドキュメントの読み込みの機能を実装。
    - ダミーMDテキストの廃止。

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,49 +1,72 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod args;
 mod document;
 mod utils;
 
+use clap::Parser;
+use document::Document;
+use tauri::State;
+
 fn main() {
-    init_log();
-    log::info!("logging started");
-    run()
+    let args = args::Args::parse();
+    if args.log || args.debug {
+        init_log(args.debug);
+    }
+    let doc = Document::new();
+    if let Some(file_name) = args.file_name {
+        match file_name.try_exists() {
+            Ok(exists) => {
+                if !exists {
+                    log::error!("指定されたファイルが存在しません。{:?}", file_name);
+                    std::process::exit(1);
+                }
+            }
+            Err(e) => {
+                log::error!("ファイルの存在確認に失敗しました。OSエラー:{}", e);
+                std::process::exit(1);
+            }
+        }
+        doc.set_file_path(file_name);
+    }
+    match doc.file_path() {
+        Some(path) => log::info!("ファイル名: {:?}", path),
+        None => log::info!("ファイル名は指定されていません。"),
+    }
+
+    run(doc)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
-    eprintln!("Tauri application is starting...");
+pub fn run(document: document::Document) {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .manage(document)
         .invoke_handler(tauri::generate_handler![contents,])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn contents() -> String {
-    let markdown = r#"
-# タイトル1
-## タイトル2
-　こんな感じで打ったらどうよ。preでないでね。
-
-* 箇条書きもしてみる。
-* もう一つ。
-
-さて、**強調**はどう?ちゃんと*斜体*もでるかしら?
-    "#;
-    let parser = pulldown_cmark::Parser::new_ext(markdown, pulldown_cmark::Options::all());
-    let mut output = String::new();
-    pulldown_cmark::html::push_html(&mut output, parser);
-    output
+async fn contents(document: State<'_, Document>) -> Result<String, String> {
+    document.html_contents().await.map_err(|e| {
+        log::error!("HTMLコンテンツの取得に失敗: {}", e);
+        e.to_string()
+    })
 }
 
 /// ロギング機構の初期設定
 /// ログファイルは、設定ディレクトリ内の`mdviewer.log`及び、標準エラー出力に出力される。
-fn init_log() {
+fn init_log(debug: bool) {
     let mut log_file_name = utils::config_path();
     log_file_name.push("mdviewer.log");
     let log_file = fern::log_file(log_file_name).expect("Failed to create log file");
+    let log_level = if debug {
+        log::LevelFilter::Debug
+    } else {
+        log::LevelFilter::Info
+    };
 
     fern::Dispatch::new()
         .format(|out, message, record| {
@@ -56,7 +79,7 @@ fn init_log() {
                 message
             ))
         })
-        .level(log::LevelFilter::Debug)
+        .level(log_level)
         .chain(std::io::stderr())
         .chain(log_file)
         .apply()
--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,12 @@ import Contents from "./Contents";
 export default function App() {
 
   return (
-      <Container width="100dvw" height="100dvh" margin="0" padding="0" gap="0">
+      <Container width="100dvw" height="100dvh" margin="0" 
+          padding="0" gap="0" overflow="hidden">
           <Container bg="#FFBA84" height="4em" gap="0" padding="0">
               <Toolbar />
           </Container>
-          <Box height="calc(100% - 4em)" >
+          <Box height="calc(100% - 4em)"  padding="1em" overflowY="auto">
               <Contents />
           </Box>
       </Container>
